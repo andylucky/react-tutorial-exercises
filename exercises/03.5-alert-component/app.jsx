1157c82fe2546343993d2d0d8643c6d71d3f0825
andylucky <3726817+andylucky@users.noreply.github.com>
2025-03-28 08:15:38 +0000

    ejercicios react 4geeks

--- exercises/03.5-alert-component/app.jsx
+++ exercises/03.5-alert-component/app.jsx
@@ -17,6 +17,7 @@ Alert.propTypes = {
 	text: PropTypes.string,
 };
 
+
 // Here is where the <Alert /> component is being used, you don't have to edit this part
 // But it helps you understand what properties the component is using
 ReactDOM.render(<Alert text="OMG! Something really bad has happened!" />, document.querySelector("#myDiv"));
